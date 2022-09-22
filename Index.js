9e9bbe647618dd46d6a3e3a25171584a1b90cc0c
rubendelar <delolmoruben@gmail.com>
2022-09-22 17:06:21 +0200

    dijous

--- Index.js
+++ Index.js
@@ -12,3 +12,45 @@ document.querySelectorAll(".nav-link").forEach((n) =>
     navMenu.classList.remove("active");
   })
 );
+
+/*FETCH*/
+
+const web = "https://jsonplaceholder.typicode.com/posts?_limit=3";
+
+function getProjects() {
+  fetch(web)
+    .then((item) => item.json())
+    .then((item2) => {
+      let infoApi = "";
+      item2.forEach((post) => {
+        infoApi += `
+      <div role="listitem" class="collection-item"> 
+        <div class="project-card">
+                <div class="project-image">
+                  <img
+                    src="./project-assets/projects-section/${post.id}.jpg"
+                    alt="project-${post.id}"
+                    class="project-card-image"
+                  />
+                </div>
+                <div class="project-content">
+                  <div class="project-text">
+                    <div class="project-title">${post.title.slice(0, 20)}</div>
+                    <div class="project-title project-description">
+                    ${post.body.slice(0, 20)}
+                    </div>
+                    <a href="./projects.html" target="_blank" class="project-link"
+                      >Learn More</a
+                    >
+                  </div>
+                </div>
+              </div>
+            </div>
+        `;
+      });
+      document.querySelector(".collection-list").innerHTML = infoApi;
+    })
+    .catch((error) => console.log(error));
+}
+
+getProjects();
---
+++ contact.js
@@ -0,0 +1,58 @@
+document.querySelector("#email-form").addEventListener("submit", addPost);
+
+const url = "https://jsonplaceholder.typicode.com/comments";
+
+function addPost(preventForm) {
+  preventForm.preventDefault();
+  let name = document.querySelector("#name").value;
+  let email = document.querySelector("#name-2").value;
+  let phone = document.querySelector("#name-3").value;
+  let message = document.querySelector("#field").value;
+
+  //   fetch().then().then().then().catch();
+  console.log(`Estic a la funcio`);
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json, text/plain",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+      phone: phone,
+      body: message,
+    }),
+  })
+    .then((response) => response.json())
+    .then((formulario) => {
+      console.log(formulario);
+    })
+    .then(() => {
+      let message = "";
+      message += `
+        
+      <div class="w-form-done">
+      <p>Thank you! Your submission has been received!</p>
+      </div>
+
+        `;
+      document.querySelector(".w-form-done").innerHTML = message;
+
+      document.querySelector("#name").value = "";
+      document.querySelector("#name-2").value = "";
+      document.querySelector("#name-3").value = "";
+      document.querySelector("#field").value = "";
+    })
+    .catch(() => {
+      let message = "";
+      message += `
+        
+      <div class="w-form-fail">
+      <p>Oops! Something went wrong while submitting the form.</p>
+      </div>
+
+        `;
+      document.querySelector(".w-form-fail").innerHTML = message;
+    });
+}
